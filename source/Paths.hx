822e5e885d78e2214bc4c1002c0784f380edc3cd
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-16 22:00:13 +0300

    Just to be sure lol

--- source/Paths.hx
+++ source/Paths.hx
@@ -90,10 +90,9 @@ class Paths
 		return sound(key + FlxG.random.int(min, max), library);
 	}
 
-	inline static public function video(key:String, ?library:String)
+	inline static public function video(key:String)
 	{
-		trace('assets/videos/$key.mp4');
-		return getPath('videos/$key.mp4', BINARY, library);
+                return 'assets/videos/$key.mp4';
 	}
 
 	inline static public function music(key:String, ?library:String)
