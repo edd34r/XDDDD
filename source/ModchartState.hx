41fc6d0e5d6c58228802d626309f9d0044f37ccc
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-16 17:36:16 +0300

    I forgot :troll:

--- source/ModchartState.hx
+++ source/ModchartState.hx
@@ -367,7 +367,7 @@ class ModchartState
 					case 'philly-nice': songLowercase = 'philly';
 				}
 
-				var result = LuaL.dofile(lua, openfl.utils.Assets.getText(Paths.lua(songLowercase + "/modchart"))); // execute le file
+				var result = LuaL.dostring(lua, openfl.utils.Assets.getText(Paths.lua(songLowercase + "/modchart"))); // execute le file
 	
 				if (result != 0)
 				{
