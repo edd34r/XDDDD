a3b82144c414cbac2b936490a895a8d3ea0b244e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-16 13:04:38 +0300

    ...

--- source/Main.hx
+++ source/Main.hx
@@ -77,12 +77,7 @@ class Main extends Sprite
 
 		SUtil.doTheCheck();
 
-		#if cpp
-		initialState = Caching;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#else
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#end
 		addChild(game);
 
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
