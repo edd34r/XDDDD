37ac6e6d20658539cdaea3facc6be97a3e6e2ace
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-17 22:15:16 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -63,6 +63,9 @@ class StoryMenuState extends MusicBeatState
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		#if windows
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Story Mode Menu", null);
